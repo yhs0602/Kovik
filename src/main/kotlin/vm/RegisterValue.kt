2146e4c475cdfa49a59fac4c276b3b70a5a1090f
yhs0602 <jourhyang123@gmail.com>
2024-05-01 16:12:24 +0900

    :sparkles: Implement const

--- src/main/kotlin/vm/RegisterValue.kt
+++ src/main/kotlin/vm/RegisterValue.kt
@@ -1,10 +1,8 @@
 package com.yhs0602.vm
 
-import com.yhs0602.dex.TypeId
-
 sealed class RegisterValue {
     data class Int(val value: kotlin.Int) : RegisterValue()
-    data class StringRef(val value: String) : RegisterValue()
-    data class ClassRef(val value: TypeId) : RegisterValue()
+    data class StringRef(val index: kotlin.Int) : RegisterValue()
+    data class ClassRef(val index: kotlin.Int) : RegisterValue()
     data class ArrayRef(val value: kotlin.Array<RegisterValue>) : RegisterValue()
 }
--- src/main/kotlin/vm/instruction/ConstInstruction.kt
+++ src/main/kotlin/vm/instruction/ConstInstruction.kt
@@ -3,80 +3,86 @@ package com.yhs0602.vm.instruction
 import com.yhs0602.dex.CodeItem
 import com.yhs0602.vm.Frame
 import com.yhs0602.vm.Memory
+import com.yhs0602.vm.RegisterValue
 
 class Const4(pc: Int, code: CodeItem) : Instruction._11n(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vA] = RegisterValue.Int(LB)
+        return pc + insnLength
     }
 }
 
 class Const16(pc: Int, code: CodeItem) : Instruction._21s(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vAA] = RegisterValue.Int(LBBBB)
+        return pc + insnLength
     }
 }
 
 class Const32(pc: Int, code: CodeItem) : Instruction._31i(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vAA] = RegisterValue.Int(BBBBhi shl 16 or BBBBlo)
+        return pc + insnLength
     }
 }
 
-class ConstHigh16(pc: Int, code: CodeItem) : Instruction._51l(pc, code) {
+class ConstHigh16(pc: Int, code: CodeItem) : Instruction._21h(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vAA] = RegisterValue.Int(LBBBB shl 16)
+        return pc + insnLength
     }
 }
 
 class ConstWide16(pc: Int, code: CodeItem) : Instruction._21s(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vAA] = RegisterValue.Int(LBBBB)
+        frame.registers[vAA + 1] = RegisterValue.Int(0)
+        return pc + insnLength
     }
 }
 
 class ConstWide32(pc: Int, code: CodeItem) : Instruction._31i(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vAA] = RegisterValue.Int(BBBBhi shl 16 or BBBBlo)
+        frame.registers[vAA + 1] = RegisterValue.Int(0)
+        return pc + insnLength
     }
 }
 
 class ConstWide64(pc: Int, code: CodeItem) : Instruction._51l(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        // FIXME
+        frame.registers[vAA] = RegisterValue.Int(BBBBhi shl 16 or BBBBlo)
+        frame.registers[vAA + 1] = RegisterValue.Int(BBBBhii shl 16 or BBBBhiii)
+        return pc + insnLength
     }
 }
 
-class ConstWideHigh16(pc: Int, code: CodeItem) : Instruction._21c(pc, code) {
+class ConstWideHigh16(pc: Int, code: CodeItem) : Instruction._21h(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vAA] = RegisterValue.Int(LBBBB shl 16)
+        frame.registers[vAA + 1] = RegisterValue.Int(0)
+        return pc + insnLength
     }
 }
 
 class ConstString(pc: Int, code: CodeItem) : Instruction._21c(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vAA] = RegisterValue.StringRef(KindBBBB)
+        return pc + insnLength
     }
 }
 
-class ConstStringJumbo(pc: Int, code: CodeItem) : Instruction._21c(pc, code) {
+class ConstStringJumbo(pc: Int, code: CodeItem) : Instruction._31c(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vAA] = RegisterValue.StringRef(KindBBBBhi shl 16 or KindBBBBlo)
+        return pc + insnLength
     }
 }
 
 class ConstClass(pc: Int, code: CodeItem) : Instruction._21c(pc, code) {
     override fun execute(pc: Int, frame: Frame, memory: Memory): Int {
-        frame.registers[vA] = vB
-        return pc + 2
+        frame.registers[vAA] = RegisterValue.ClassRef(KindBBBB)
+        return pc + insnLength
     }
 }
